6e9fa0f090128f8028335fd522ab2c5d92b867fb
Neil Flatley <n.flatley@zengenti.com>
2023-07-18 01:55:00 +0100

    feat: split v4l2loopback and ffmpeg services so ffmpeg can be started as user

---
+++ enable-v4l2loopback.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+modprobe v4l2loopback
--- httpreversecam.sh
+++ httpreversecam.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-modprobe v4l2loopback
 ffmpeg -hide_banner -avoid_negative_ts make_zero -fflags nobuffer -flags low_delay -strict experimental -fflags +genpts+discardcorrupt -use_wallclock_as_timestamps 1 -f mjpeg -i http://192.168.12.1:8082 -an -r 10 -vf "hflip,scale=512:-1" -pix_fmt yuv420p -f v4l2 /dev/video0
