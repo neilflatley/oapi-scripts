c55bb854d86ce65a10293189662600c8fd05380e
Neil Flatley <n.flatley@zengenti.com>
2023-07-18 01:55:36 +0100

    feat: add startup script and service to call it on system startup

---
+++ startup.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# update scripts directory
+git pull || echo "ERROR: git pull failed"
+
+# add execute permission to scripts
+chmod +x *.sh
+
+# cp systemd files
+cp ./systemd /etc/systemd
+
+# reload systemd daemon
+systemctl daemon-reload 
+
+# ensure the service is enabled to start this script on next reboot
+systemctl enable oapi-scripts.service
