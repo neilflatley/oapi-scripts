d3334dfd10934091aa24407702c9f565908a5670
Neil Flatley <neilflatley@hotmail.com>
2023-07-27 22:05:48 +0100

    fix: remove hflip video filter and add sleep before calling ha service

--- httpreversecam.sh
+++ httpreversecam.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+# Give the wifi some time to start
+sleep 12
+
 # Call Home Assistant to turn on Surveillance system
 curl \
   -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiZjA0NDhjZTYwYzQ0NzkwOTE5MzQ0OTExZGE1ZjZjYSIsImlhdCI6MTY4OTk5OTgxMCwiZXhwIjoyMDA1MzU5ODEwfQ.zQkeyO2-YvvpTIvqyy_JVJbzRgEp6IKy79BMWl0m_VQ" \
@@ -14,7 +17,7 @@ sleep 12
 #ffmpeg -hide_banner -avoid_negative_ts make_zero -fflags nobuffer -flags low_delay -strict experimental -fflags +genpts+discardcorrupt -use_wallclock_as_timestamps 1 -f mjpeg -i http://192.168.12.1:8082 -an -r 10 -vf "hflip,scale=512:-1" -pix_fmt yuv420p -f v4l2 /dev/video0
 
 # frigate RTSP H264 stream to v4l2 /dev/video0
-cmd="ffmpeg -hide_banner -allowed_media_types video -fflags nobuffer -flags low_delay -user_agent oapi/ffmpeg -rtsp_transport tcp -i rtsp://192.168.12.1:8554/rear -vf hflip -f v4l2 /dev/video0"
+cmd="ffmpeg -hide_banner -allowed_media_types video -fflags nobuffer -flags low_delay -user_agent oapi/ffmpeg -rtsp_transport tcp -i rtsp://192.168.12.1:8554/rear  -f v4l2 /dev/video0"
 
 echo $cmd
 $cmd
